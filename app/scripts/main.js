60ef68bea821bd54e28e22ef45b6973c29c46996
Fernando Almeida <fernando@fernandoalmeida.net>
2014-02-11 02:12:23 -0300

    Backbone routes and initial configurations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-/* global console, $ */
+/* global console, $, Backbone */
+
+var BackboneStore = Backbone.Router.extend({
+
+  greeting: '',
+
+  routes: {
+    "": 'home',
+    "store": 'list',
+    "store/:item": 'item'
+  },
+
+  home: function(){
+    this.hideAll();
+    $('.home').show();
+  },
+
+  list: function() {
+    this.hideAll();
+    $('.list').show();
+  },
+
+  item: function() {
+    this.hideAll();
+    $('.item').show();
+  },
+
+  hideAll: function() {
+    $('.main > div').hide();
+  }
+
+});
+
+var app = new BackboneStore();
+
+$(function(){
+  Backbone.history.start();
+});
